1ffe19c335d8a1417917c959b25b1503a967690e
Uday083 <udaybkumar083@outlook.com>
2026-04-02 12:36:13 +0530

    Added test.config.ts file

---
+++ test.config.ts
@@ -0,0 +1,8 @@
+export class TestConfig {
+    appUrl= "https://www.saucedemo.com/"
+    user= "standard_user"
+    password= "secret_sauce"
+    product_name= "Sauce Labs Backpack"
+    product_qty= "1"
+    product_price= "$29.99"
+}
