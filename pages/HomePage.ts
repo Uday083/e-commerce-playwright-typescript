487d4d1853eafa92d5dd9259096b452d2aa53463
Uday083 <udaybkumar083@outlook.com>
2026-04-02 17:11:11 +0530

    Updated the HomePage, Login, playwright.config.ts, test.spec.ts

--- pages/HomePage.ts
+++ pages/HomePage.ts
@@ -1,1 +1,32 @@
 //only the elements of the Home page without the products. This will include Hamburger menu, filters, Cart icon, Social media icons, etc...
+
+import { Page, expect, Locator } from '@playwright/test';
+export class Home {
+
+    private readonly page: Page;    
+    //locators
+    private readonly hamburgerMenu: Locator;
+    private readonly filterButton: Locator;
+    private readonly cartIcon: Locator
+    private readonly twitterIcon: Locator;
+    private readonly facebookIcon: Locator;
+    //constructor
+    constructor(page: Page) {
+        this.page = page;
+        this.hamburgerMenu = page.locator('#react-burger-menu-btn');
+        this.filterButton = page.locator('.product_sort_container');
+        this.cartIcon = page.locator('.shopping_cart_link');
+        this.twitterIcon = page.locator('.social_twitter');
+        this.facebookIcon = page.locator('.social_facebook');
+    }
+    //Action methods
+    async isPageExist(){
+
+        let title=await this.page.title();
+        if (title){
+            return true;
+        }
+        else{            return false;
+        }
+    }
+}
--- pages/Login.ts
+++ pages/Login.ts
@@ -1,1 +1,41 @@
 // This page includes the elements of the Login page, scenarios of right password, wrong password, and empty password, etc.
+import { Page, expect, Locator } from '@playwright/test';
+import { clearScreenDown } from 'node:readline';
+
+export class LoginPage {
+
+    private readonly page: Page;
+
+    //locators
+    private readonly usernameInput: Locator;
+    private readonly passwordInput: Locator;
+    private readonly loginButton: Locator;
+
+    //constructor
+    constructor(page: Page) {
+        this.page = page;
+        this.usernameInput = page.locator('#user-name');
+        this.passwordInput = page.locator('#password');
+        this.loginButton = page.locator('#login-button');
+    }
+
+    //Action methods
+    async isPageExist()
+    {
+        let title=await this.page.title();
+        if (title){
+            return true;
+        }
+        else
+        {            
+            return false;
+        }
+    }
+
+    async loginToApp(username: string, password: string) 
+    {
+        await this.usernameInput.fill(username);
+        await this.passwordInput.fill(password);
+        await this.loginButton.click();
+    }
+}
